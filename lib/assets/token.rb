c51ffbb22c1335dd573ceccbfff330e3c7f17675
Zach Fountain <zach.fountain@gmail.com>
2015-06-23 23:46:24 -0500

    Adding code comments to reconciliations index and token.rb

--- lib/assets/token.rb
+++ lib/assets/token.rb
@@ -2,12 +2,12 @@ require 'httparty'
 require 'json'
 
 class GitHub
-
+    # Unique ID and token
     API_CLIENT_ID = 'b698ad5804c807d9e188'
     API_CLIENT_SECRET = '89060fce512d1042d168c3e71504ae35208cef1b'
       
-      include HTTParty
-      headers "User-Agent" => "GA2Project3"
+      include HTTParty # Ruby gem for API
+      headers "User-Agent" => "GA2Project3" # Identifies originator of request
       basic_auth "api-padawan", "GitHubPassw0rd"
 
       def create_token
@@ -19,7 +19,7 @@ class GitHub
 
     	end
 
-
+      # Posts "requires authentication" message in json
       endpoint = "https://api.github.com/authorizations"
       	
       	post_body = {}
